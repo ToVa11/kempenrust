7371bdf837744415a3152198c41ade0decd5c0ba
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-03-28 11:50:50 +0100

    Aanmaken repositories #94

---
+++ src/main/java/pr4/t1/kempenrust/repository/BoekingDetailRepository.java
@@ -0,0 +1,12 @@
+package pr4.t1.kempenrust.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoekingDetailRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+}
---
+++ src/main/java/pr4/t1/kempenrust/repository/BoekingRepository.java
@@ -0,0 +1,12 @@
+package pr4.t1.kempenrust.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoekingRepository {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+}
