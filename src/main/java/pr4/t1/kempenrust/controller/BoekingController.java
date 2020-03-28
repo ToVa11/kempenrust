8e93540eed657e8649e283cb610c89507af96d71
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-03-28 15:48:53 +0100

    Navigatiebalk website aanmaken #88

--- src/main/java/pr4/t1/kempenrust/controller/BoekingController.java
+++ src/main/java/pr4/t1/kempenrust/controller/BoekingController.java
@@ -1,9 +1,13 @@
 package pr4.t1.kempenrust.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoekingController {
 //    Hier komen alle methodes die iets te maken hebben met boekingen
-
+    @RequestMapping("/reserveren")
+    public String Reserveren() {
+        return "layouts/boeking/reserveren";
+    }
 }
