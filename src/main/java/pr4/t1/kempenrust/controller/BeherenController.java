8c99848f7cb02ef17a6da6af5d293547d8a86a06
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-03-28 11:34:47 +0100

    Aanmaken Controllers

---
+++ src/main/java/pr4/t1/kempenrust/controller/BeherenController.java
@@ -0,0 +1,8 @@
+package pr4.t1.kempenrust.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BeherenController {
+//    Hier komen alle methodes in die iets te maken hebben met het beheren (CRUD) van het hotel
+}
--- src/main/java/pr4/t1/kempenrust/controller/BoekingController.java
+++ src/main/java/pr4/t1/kempenrust/controller/BoekingController.java
@@ -3,6 +3,7 @@ package pr4.t1.kempenrust.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class HomeController {
-//      Hier komen de rest van de methodes (standaard)
+public class BoekingController {
+//    Hier komen alle methodes die iets te maken hebben met boekingen
+
 }
