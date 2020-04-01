a993a68fdcdbe21d6f5f4e4485e7f9da0bde66fe
Tom Vanelven <tom.vanelven@hotmail.com>
2020-04-01 13:44:16 +0200

    adds front- en backend functionaliteiten voor een reserveringlijst

--- src/main/java/pr4/t1/kempenrust/controller/BoekingController.java
+++ src/main/java/pr4/t1/kempenrust/controller/BoekingController.java
@@ -1,10 +1,19 @@
 package pr4.t1.kempenrust.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pr4.t1.kempenrust.model.BoekingDetail;
+import pr4.t1.kempenrust.repository.BoekingDetailRepository;
+
+import java.util.ArrayList;
 
 @Controller
 public class BoekingController {
+    @Autowired
+    BoekingDetailRepository boekingDetailRepository;
+
 //    Hier komen alle methodes die iets te maken hebben met boekingen
     @RequestMapping("/reserveren")
     public String Reserveren() {
@@ -12,7 +21,12 @@ public class BoekingController {
     }
 
     @RequestMapping("/reserveringen")
-    public String Reserveringen() {
+    public String Reserveringen(Model model) {
+
+        ArrayList<BoekingDetail> details = boekingDetailRepository.getAllDetailsFromToday();
+
+        model.addAttribute("details",details);
+
         return "layouts/boeking/reserveringen";
     }
 
--- src/main/java/pr4/t1/kempenrust/repository/BoekingDetailRepository.java
+++ src/main/java/pr4/t1/kempenrust/repository/BoekingDetailRepository.java
@@ -2,11 +2,54 @@ package pr4.t1.kempenrust.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
+import pr4.t1.kempenrust.model.Boeking;
+import pr4.t1.kempenrust.model.BoekingDetail;
+import pr4.t1.kempenrust.model.Kamer;
+import pr4.t1.kempenrust.model.Klant;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 @Repository
 public class BoekingDetailRepository {
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    public ArrayList<BoekingDetail> getAllDetailsFromToday() {
+
+        ArrayList<BoekingDetail> details = new ArrayList<>();
+
+        Date datumVan = new Date(new java.util.Date().getTime());
+
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet("SELECT * FROM boekingDetails INNER JOIN " +
+                "(boekingen INNER JOIN klanten ON boekingen.klantId = klanten.klantId) ON boekingDetails.boekingId = boekingen.boekingID " +
+                "INNER JOIN kamers ON boekingDetails.kamerId = kamers.kamerId WHERE boekingen.datumVan > ? ORDER BY boekingen.datumVan", Date.valueOf(LocalDate.now()));
+        
+        while(rowSet.next()) {
+            BoekingDetail detail = new BoekingDetail();
+            Klant klant = new Klant();
+            Boeking boeking = new Boeking();
+            Kamer kamer = new Kamer();
+
+            klant.setEmail(rowSet.getString("email"));
+            klant.setNaam(rowSet.getString("naam"));
+
+            boeking.setDatumVan(rowSet.getDate("datumVan"));
+            boeking.setDatumTot(rowSet.getDate("datumTot"));
+
+            kamer.setKamerNummer(rowSet.getInt("kamerNummer"));
+
+            boeking.setKlant(klant);
+            detail.setKamer(kamer);
+            detail.setBoeking(boeking);
+
+            details.add(detail);
+        }
+
+        return details;
+    }
 }
