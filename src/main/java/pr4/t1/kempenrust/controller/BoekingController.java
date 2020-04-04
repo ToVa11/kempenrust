5305fa505064a71c90da02b0364a664fee60b1b3
Sam Ceustermans <ceustermanssam1989@gmail.com>
2020-04-04 15:31:24 +0200

    Backend: Klant zoekt naar kamers tijdens opgegeven periode #99

--- src/main/java/pr4/t1/kempenrust/controller/BoekingController.java
+++ src/main/java/pr4/t1/kempenrust/controller/BoekingController.java
@@ -5,19 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pr4.t1.kempenrust.model.BoekingDetail;
+import pr4.t1.kempenrust.model.Kamer;
 import pr4.t1.kempenrust.model.VerblijfsKeuze;
 import pr4.t1.kempenrust.repository.BoekingDetailRepository;
+import pr4.t1.kempenrust.repository.KamerRepository;
 import pr4.t1.kempenrust.repository.VerblijfsKeuzeRepository;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 @Controller
 public class BoekingController {
     @Autowired
     BoekingDetailRepository boekingDetailRepository;
-
     @Autowired
     VerblijfsKeuzeRepository verblijfsKeuzeRepository;
+    @Autowired
+    KamerRepository kamerRepository;
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
 //    Hier komen alle methodes die iets te maken hebben met boekingen
     @RequestMapping("/reserveren")
@@ -26,6 +34,23 @@ public class BoekingController {
         ArrayList<VerblijfsKeuze> verblijfsKeuzes = verblijfsKeuzeRepository.getAllVerblijfsKeuzes();
 
         model.addAttribute("verblijfskeuzes", verblijfsKeuzes);
+
+        return "layouts/boeking/reserveren";
+    }
+
+    @RequestMapping("/zoek-kamers")
+    public String ZoekKamers(Model model, HttpServletRequest request) {
+
+        LocalDate datumAankomst = LocalDate.parse(request.getParameter("datumAankomst"), formatter);
+        LocalDate datumVertrek = request.getParameter("datumVertrek") != ""
+                ? LocalDate.parse(request.getParameter("datumVertrek"), formatter)
+                : null;
+        int aantalPersonen = Integer.parseInt(request.getParameter("aantalPersonen"));
+        int verblijfsKeuzeID = Integer.parseInt(request.getParameter("keuzeArrangement"));
+
+        ArrayList<Kamer> vrijeKamers = kamerRepository.getAllAvailableRooms(verblijfsKeuzeID, datumAankomst, datumVertrek);
+
+        model.addAttribute("vrijeKamers", vrijeKamers);
 
         return "layouts/boeking/reserveren";
     }
--- src/main/java/pr4/t1/kempenrust/repository/KamerRepository.java
+++ src/main/java/pr4/t1/kempenrust/repository/KamerRepository.java
@@ -1,12 +1,66 @@
 package pr4.t1.kempenrust.repository;
 
+import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
+import pr4.t1.kempenrust.model.Kamer;
+import pr4.t1.kempenrust.model.KamerType;
+import pr4.t1.kempenrust.model.Prijs;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 @Repository
 public class KamerRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    public ArrayList<Kamer> getAllAvailableRooms(int verblijfskeuzeID, LocalDate datumAankomst, LocalDate datumVertrek) {
+        ArrayList<Kamer> kamers = new ArrayList<>();
+
+        Date datumVan = Date.valueOf(datumAankomst);
+        Date datumTot = (datumVertrek != null) ? Date.valueOf(datumVertrek) : null;
+
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet("" +
+                "SELECT * " +
+                "FROM Kamers INNER JOIN " +
+                        "(KamerTypes INNER JOIN Prijzen " +
+                                "ON KamerTypes.KamerTypeID = Prijzen.KamerTypeID) " +
+                        "ON Kamers.KamerTypeID = KamerTypes.KamerTypeID " +
+                "WHERE VerblijfsKeuzeID = ? AND KamerID NOT IN " +
+                        "(SELECT KamerID " +
+                        "FROM BoekingDetails INNER JOIN Boekingen " +
+                                "ON BoekingDetails.BoekingID = Boekingen.BoekingID " +
+                        "WHERE DatumVan >= ? AND DatumTot <= ?)",
+                verblijfskeuzeID,
+                datumAankomst,
+                datumVertrek);
+
+        while (rowSet.next()) {
+            Kamer kamer = new Kamer();
+            KamerType kamerType = new KamerType();
+            Prijs prijs = new Prijs();
+
+            kamerType.setKamerTypeID(rowSet.getInt("KamerTypeID"));
+            kamerType.setOmschrijving(rowSet.getString("Omschrijving"));
+
+            kamer.setKamerID(rowSet.getInt("KamerID"));
+            kamer.setKamerTypeID(rowSet.getInt("KamerTypeID"));
+            kamer.setKamerType(kamerType);
+            kamer.setKamerNummer(rowSet.getInt("KamerNummer"));
+
+            prijs.setPrijsID(rowSet.getInt("PrijsID"));
+            prijs.setKamerTypeID(rowSet.getInt("KamerTypeID"));
+            prijs.setKamerType(kamerType);
+            prijs.setVerblijfsKeuzeID(rowSet.getInt("VerblijfsKeuzeID"));
+            prijs.setPrijsPerPersoon(rowSet.getBigDecimal("PrijsPerPersoon"));
+            prijs.setDatumVanaf(rowSet.getDate("DatumVanaf"));
+
+            kamers.add(kamer);
+        }
+        return kamers;
+    }
 }
